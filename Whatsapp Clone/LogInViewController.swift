700abd445c810794e3ca8df5ed589b13bf202210
Gene K. Essel <eugene2essel@gmail.com>
2017-11-26 02:31:03 -0500

    add user login to chat

--- Whatsapp Clone/LogInViewController.swift
+++ Whatsapp Clone/LogInViewController.swift
@@ -6,6 +6,7 @@
 
 
 import UIKit
+import Firebase
 
 
 class LogInViewController: UIViewController {
@@ -28,7 +29,16 @@ class LogInViewController: UIViewController {
 
         
         //TODO: Log in the user
-
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) {
+            (user, error) in
+            
+            if error != nil {
+                print(error!)
+            }else{
+                print("Login success")
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
     
     }
     
